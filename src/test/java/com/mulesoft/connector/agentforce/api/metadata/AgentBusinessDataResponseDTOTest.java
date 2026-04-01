550d69451dcdc9abd6704f71e6ae4ebfd4aba4f8
ayush-salesforce <162944854+ayush-salesforce@users.noreply.github.com>
2026-04-01 13:34:40 +0530

    Feature/certification checklist fixes (#138)
    * Changes related to certification checklist
    
    * Update tests and missing null check
    
    * revert pom changes
    
    * update dependency for vulnerability
    
    * Remove unnecessary demo files

--- src/test/java/com/mulesoft/connector/agentforce/api/metadata/AgentBusinessDataResponseDTOTest.java
+++ src/test/java/com/mulesoft/connector/agentforce/api/metadata/AgentBusinessDataResponseDTOTest.java
@@ -1,51 +1,48 @@
 package com.mulesoft.connector.agentforce.api.metadata;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
+import java.util.Collections;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class AgentBusinessDataResponseDTOTest {
 
-  private ObjectMapper objectMapper;
-
-  @BeforeEach
-  void setUp() {
-    objectMapper = new ObjectMapper();
-  }
-
   @Test
   void testEquals_SameObject() {
-    AgentBusinessDataResponseDTO.BusinessDataMessage msg = new AgentBusinessDataResponseDTO.BusinessDataMessage("Inform");
-    AgentBusinessDataResponseDTO dto = new AgentBusinessDataResponseDTO(Arrays.asList(msg));
+    AgentBusinessDataResponseDTO.BusinessDataMessage msg =
+        new AgentBusinessDataResponseDTO.BusinessDataMessage.Builder("Inform").build();
+    AgentBusinessDataResponseDTO dto = new AgentBusinessDataResponseDTO(Collections.singletonList(msg));
     assertEquals(dto, dto);
   }
 
   @Test
   void testEquals_NullObject() {
-    AgentBusinessDataResponseDTO.BusinessDataMessage msg = new AgentBusinessDataResponseDTO.BusinessDataMessage("Inform");
-    AgentBusinessDataResponseDTO dto = new AgentBusinessDataResponseDTO(Arrays.asList(msg));
+    AgentBusinessDataResponseDTO.BusinessDataMessage msg =
+        new AgentBusinessDataResponseDTO.BusinessDataMessage.Builder("Inform").build();
+    AgentBusinessDataResponseDTO dto = new AgentBusinessDataResponseDTO(Collections.singletonList(msg));
     assertNotEquals(null, dto);
   }
 
   @Test
   void testEquals_DifferentClass() {
-    AgentBusinessDataResponseDTO.BusinessDataMessage msg = new AgentBusinessDataResponseDTO.BusinessDataMessage("Inform");
-    AgentBusinessDataResponseDTO dto = new AgentBusinessDataResponseDTO(Arrays.asList(msg));
-    assertNotEquals(dto, new Object());
+    AgentBusinessDataResponseDTO.BusinessDataMessage msg =
+        new AgentBusinessDataResponseDTO.BusinessDataMessage.Builder("Inform").build();
+    AgentBusinessDataResponseDTO dto = new AgentBusinessDataResponseDTO(Collections.singletonList(msg));
+    assertNotEquals(new Object(), dto);
   }
 
   @Test
   void testEquals_EqualObjects() {
-    AgentBusinessDataResponseDTO.BusinessDataMessage msg1 = new AgentBusinessDataResponseDTO.BusinessDataMessage("Inform");
-    msg1.setMessage("Hello");
+    AgentBusinessDataResponseDTO.BusinessDataMessage msg1 = new AgentBusinessDataResponseDTO.BusinessDataMessage.Builder("Inform")
+        .message("Hello")
+        .build();
     AgentBusinessDataResponseDTO dto1 = new AgentBusinessDataResponseDTO(Arrays.asList(msg1));
 
-    AgentBusinessDataResponseDTO.BusinessDataMessage msg2 = new AgentBusinessDataResponseDTO.BusinessDataMessage("Inform");
-    msg2.setMessage("Hello");
+    AgentBusinessDataResponseDTO.BusinessDataMessage msg2 = new AgentBusinessDataResponseDTO.BusinessDataMessage.Builder("Inform")
+        .message("Hello")
+        .build();
     AgentBusinessDataResponseDTO dto2 = new AgentBusinessDataResponseDTO(Arrays.asList(msg2));
 
     assertEquals(dto1, dto2);
@@ -53,10 +50,12 @@ class AgentBusinessDataResponseDTOTest {
 
   @Test
   void testEquals_DifferentMessages() {
-    AgentBusinessDataResponseDTO.BusinessDataMessage msg1 = new AgentBusinessDataResponseDTO.BusinessDataMessage("Inform");
+    AgentBusinessDataResponseDTO.BusinessDataMessage msg1 =
+        new AgentBusinessDataResponseDTO.BusinessDataMessage.Builder("Inform").build();
     AgentBusinessDataResponseDTO dto1 = new AgentBusinessDataResponseDTO(Arrays.asList(msg1));
 
-    AgentBusinessDataResponseDTO.BusinessDataMessage msg2 = new AgentBusinessDataResponseDTO.BusinessDataMessage("Inquire");
+    AgentBusinessDataResponseDTO.BusinessDataMessage msg2 =
+        new AgentBusinessDataResponseDTO.BusinessDataMessage.Builder("Inquire").build();
     AgentBusinessDataResponseDTO dto2 = new AgentBusinessDataResponseDTO(Arrays.asList(msg2));
 
     assertNotEquals(dto1, dto2);
@@ -64,12 +63,14 @@ class AgentBusinessDataResponseDTOTest {
 
   @Test
   void testHashCode_EqualObjects() {
-    AgentBusinessDataResponseDTO.BusinessDataMessage msg1 = new AgentBusinessDataResponseDTO.BusinessDataMessage("Inform");
-    msg1.setMessage("Test");
+    AgentBusinessDataResponseDTO.BusinessDataMessage msg1 = new AgentBusinessDataResponseDTO.BusinessDataMessage.Builder("Inform")
+        .message("Test")
+        .build();
     AgentBusinessDataResponseDTO dto1 = new AgentBusinessDataResponseDTO(Arrays.asList(msg1));
 
-    AgentBusinessDataResponseDTO.BusinessDataMessage msg2 = new AgentBusinessDataResponseDTO.BusinessDataMessage("Inform");
-    msg2.setMessage("Test");
+    AgentBusinessDataResponseDTO.BusinessDataMessage msg2 = new AgentBusinessDataResponseDTO.BusinessDataMessage.Builder("Inform")
+        .message("Test")
+        .build();
     AgentBusinessDataResponseDTO dto2 = new AgentBusinessDataResponseDTO(Arrays.asList(msg2));
 
     assertEquals(dto1.hashCode(), dto2.hashCode());
@@ -77,69 +78,81 @@ class AgentBusinessDataResponseDTOTest {
 
   @Test
   void testBusinessDataMessageEquals_SameObject() {
-    AgentBusinessDataResponseDTO.BusinessDataMessage msg = new AgentBusinessDataResponseDTO.BusinessDataMessage("Inform");
+    AgentBusinessDataResponseDTO.BusinessDataMessage msg =
+        new AgentBusinessDataResponseDTO.BusinessDataMessage.Builder("Inform").build();
     assertEquals(msg, msg);
   }
 
   @Test
   void testBusinessDataMessageEquals_NullObject() {
-    AgentBusinessDataResponseDTO.BusinessDataMessage msg = new AgentBusinessDataResponseDTO.BusinessDataMessage("Inform");
+    AgentBusinessDataResponseDTO.BusinessDataMessage msg =
+        new AgentBusinessDataResponseDTO.BusinessDataMessage.Builder("Inform").build();
     assertNotEquals(null, msg);
   }
 
   @Test
   void testBusinessDataMessageEquals_DifferentClass() {
-    AgentBusinessDataResponseDTO.BusinessDataMessage msg = new AgentBusinessDataResponseDTO.BusinessDataMessage("Inform");
-    assertNotEquals(msg, new Object());
+    AgentBusinessDataResponseDTO.BusinessDataMessage msg =
+        new AgentBusinessDataResponseDTO.BusinessDataMessage.Builder("Inform").build();
+    assertNotEquals(new Object(), msg);
   }
 
   @Test
   void testBusinessDataMessageEquals_EqualObjects() {
-    AgentBusinessDataResponseDTO.BusinessDataMessage msg1 = new AgentBusinessDataResponseDTO.BusinessDataMessage("Inform");
-    msg1.setMessage("Hello");
+    AgentBusinessDataResponseDTO.BusinessDataMessage msg1 = new AgentBusinessDataResponseDTO.BusinessDataMessage.Builder("Inform")
+        .message("Hello")
+        .build();
 
-    AgentBusinessDataResponseDTO.BusinessDataMessage msg2 = new AgentBusinessDataResponseDTO.BusinessDataMessage("Inform");
-    msg2.setMessage("Hello");
+    AgentBusinessDataResponseDTO.BusinessDataMessage msg2 = new AgentBusinessDataResponseDTO.BusinessDataMessage.Builder("Inform")
+        .message("Hello")
+        .build();
 
     assertEquals(msg1, msg2);
   }
 
   @Test
   void testBusinessDataMessageEquals_DifferentType() {
-    AgentBusinessDataResponseDTO.BusinessDataMessage msg1 = new AgentBusinessDataResponseDTO.BusinessDataMessage("Inform");
-    AgentBusinessDataResponseDTO.BusinessDataMessage msg2 = new AgentBusinessDataResponseDTO.BusinessDataMessage("Inquire");
+    AgentBusinessDataResponseDTO.BusinessDataMessage msg1 =
+        new AgentBusinessDataResponseDTO.BusinessDataMessage.Builder("Inform").build();
+    AgentBusinessDataResponseDTO.BusinessDataMessage msg2 =
+        new AgentBusinessDataResponseDTO.BusinessDataMessage.Builder("Inquire").build();
 
     assertNotEquals(msg1, msg2);
   }
 
   @Test
   void testBusinessDataMessageEquals_DifferentMessage() {
-    AgentBusinessDataResponseDTO.BusinessDataMessage msg1 = new AgentBusinessDataResponseDTO.BusinessDataMessage("Inform");
-    msg1.setMessage("Hello");
+    AgentBusinessDataResponseDTO.BusinessDataMessage msg1 = new AgentBusinessDataResponseDTO.BusinessDataMessage.Builder("Inform")
+        .message("Hello")
+        .build();
 
-    AgentBusinessDataResponseDTO.BusinessDataMessage msg2 = new AgentBusinessDataResponseDTO.BusinessDataMessage("Inform");
-    msg2.setMessage("Goodbye");
+    AgentBusinessDataResponseDTO.BusinessDataMessage msg2 = new AgentBusinessDataResponseDTO.BusinessDataMessage.Builder("Inform")
+        .message("Goodbye")
+        .build();
 
     assertNotEquals(msg1, msg2);
   }
 
   @Test
   void testBusinessDataMessageHashCode_EqualObjects() {
-    AgentBusinessDataResponseDTO.BusinessDataMessage msg1 = new AgentBusinessDataResponseDTO.BusinessDataMessage("Inform");
-    msg1.setMessage("Test");
+    AgentBusinessDataResponseDTO.BusinessDataMessage msg1 = new AgentBusinessDataResponseDTO.BusinessDataMessage.Builder("Inform")
+        .message("Test")
+        .build();
 
-    AgentBusinessDataResponseDTO.BusinessDataMessage msg2 = new AgentBusinessDataResponseDTO.BusinessDataMessage("Inform");
-    msg2.setMessage("Test");
+    AgentBusinessDataResponseDTO.BusinessDataMessage msg2 = new AgentBusinessDataResponseDTO.BusinessDataMessage.Builder("Inform")
+        .message("Test")
+        .build();
 
     assertEquals(msg1.hashCode(), msg2.hashCode());
   }
 
   @Test
   void testBusinessDataMessage_InformFields() {
-    AgentBusinessDataResponseDTO.BusinessDataMessage msg = new AgentBusinessDataResponseDTO.BusinessDataMessage("Inform");
-    msg.setMessage("Information message");
-    msg.setResult(Arrays.asList());
-    msg.setCitedReferences(Arrays.asList());
+    AgentBusinessDataResponseDTO.BusinessDataMessage msg = new AgentBusinessDataResponseDTO.BusinessDataMessage.Builder("Inform")
+        .message("Information message")
+        .result(Collections.emptyList())
+        .citedReferences(Collections.emptyList())
+        .build();
 
     assertEquals("Inform", msg.getType());
     assertEquals("Information message", msg.getMessage());
@@ -149,9 +162,10 @@ class AgentBusinessDataResponseDTOTest {
 
   @Test
   void testBusinessDataMessage_InquireFields() {
-    AgentBusinessDataResponseDTO.BusinessDataMessage msg = new AgentBusinessDataResponseDTO.BusinessDataMessage("Inquire");
-    msg.setMessage("Question?");
-    msg.setCollect(Arrays.asList());
+    AgentBusinessDataResponseDTO.BusinessDataMessage msg = new AgentBusinessDataResponseDTO.BusinessDataMessage.Builder("Inquire")
+        .message("Question?")
+        .collect(Collections.emptyList())
+        .build();
 
     assertEquals("Inquire", msg.getType());
     assertEquals("Question?", msg.getMessage());
@@ -160,8 +174,9 @@ class AgentBusinessDataResponseDTOTest {
 
   @Test
   void testBusinessDataMessage_ConfirmFields() {
-    AgentBusinessDataResponseDTO.BusinessDataMessage msg = new AgentBusinessDataResponseDTO.BusinessDataMessage("Confirm");
-    msg.setConfirm(Arrays.asList());
+    AgentBusinessDataResponseDTO.BusinessDataMessage msg = new AgentBusinessDataResponseDTO.BusinessDataMessage.Builder("Confirm")
+        .confirm(Collections.emptyList())
+        .build();
 
     assertEquals("Confirm", msg.getType());
     assertNotNull(msg.getConfirm());
@@ -169,9 +184,10 @@ class AgentBusinessDataResponseDTOTest {
 
   @Test
   void testBusinessDataMessage_FailureFields() {
-    AgentBusinessDataResponseDTO.BusinessDataMessage msg = new AgentBusinessDataResponseDTO.BusinessDataMessage("Failure");
-    msg.setCode("ERROR_CODE");
-    msg.setErrors(Arrays.asList("Error 1", "Error 2"));
+    AgentBusinessDataResponseDTO.BusinessDataMessage msg = new AgentBusinessDataResponseDTO.BusinessDataMessage.Builder("Failure")
+        .code("ERROR_CODE")
+        .errors(Arrays.asList("Error 1", "Error 2"))
+        .build();
 
     assertEquals("Failure", msg.getType());
     assertEquals("ERROR_CODE", msg.getCode());
@@ -181,8 +197,10 @@ class AgentBusinessDataResponseDTOTest {
 
   @Test
   void testBusinessDataMessage_SessionEndedFields() {
-    AgentBusinessDataResponseDTO.BusinessDataMessage msg = new AgentBusinessDataResponseDTO.BusinessDataMessage("SessionEnded");
-    msg.setReason("ClientRequest");
+    AgentBusinessDataResponseDTO.BusinessDataMessage msg =
+        new AgentBusinessDataResponseDTO.BusinessDataMessage.Builder("SessionEnded")
+            .reason("ClientRequest")
+            .build();
 
     assertEquals("SessionEnded", msg.getType());
     assertEquals("ClientRequest", msg.getReason());
@@ -191,8 +209,9 @@ class AgentBusinessDataResponseDTOTest {
   @Test
   void testBusinessDataMessage_ProgressIndicatorFields() {
     AgentBusinessDataResponseDTO.BusinessDataMessage msg =
-        new AgentBusinessDataResponseDTO.BusinessDataMessage("ProgressIndicator");
-    msg.setIndicatorType("Processing");
+        new AgentBusinessDataResponseDTO.BusinessDataMessage.Builder("ProgressIndicator")
+            .indicatorType("Processing")
+            .build();
 
     assertEquals("ProgressIndicator", msg.getType());
     assertEquals("Processing", msg.getIndicatorType());
@@ -200,8 +219,10 @@ class AgentBusinessDataResponseDTOTest {
 
   @Test
   void testBusinessDataMessage_EscalateFields() {
-    AgentBusinessDataResponseDTO.BusinessDataMessage msg = new AgentBusinessDataResponseDTO.BusinessDataMessage("Escalate");
-    msg.setTargets(Arrays.asList());
+    AgentBusinessDataResponseDTO.BusinessDataMessage msg =
+        new AgentBusinessDataResponseDTO.BusinessDataMessage.Builder("Escalate")
+            .targets(Collections.emptyList())
+            .build();
 
     assertEquals("Escalate", msg.getType());
     assertNotNull(msg.getTargets());
@@ -209,8 +230,9 @@ class AgentBusinessDataResponseDTOTest {
 
   @Test
   void testBusinessDataMessage_ErrorFields() {
-    AgentBusinessDataResponseDTO.BusinessDataMessage msg = new AgentBusinessDataResponseDTO.BusinessDataMessage("Error");
-    msg.setError("INTERNAL_ERROR");
+    AgentBusinessDataResponseDTO.BusinessDataMessage msg = new AgentBusinessDataResponseDTO.BusinessDataMessage.Builder("Error")
+        .error("INTERNAL_ERROR")
+        .build();
 
     assertEquals("Error", msg.getType());
     assertEquals("INTERNAL_ERROR", msg.getError());
